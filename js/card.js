30a092dd0b45895cebf30b1f1f9e0ea8bf766f2a
Erica Nafziger and Jonathan Thom <jonathan@>
2017-01-02 11:02:27 -0800

    create randomize prototype

--- js/card.js
+++ js/card.js
@@ -1,6 +1,11 @@
-function Card(front, back) {
-  this.front = front;
+function Card(back) {
+  this.front;
   this.back = back;
 }
+
+Card.prototype.randomize = function(images) {
+  var random = images[Math.floor(Math.random() * images.length)];
+  this.front = random;
+};
 
 exports.cardModule = Card;
--- js/memory-interface.js
+++ js/memory-interface.js
@@ -1,14 +1,16 @@
 var Card = require('./../js/card.js').cardModule;
 
+var images = ['<img src="img/bm1.jpg">', '<img src="img/bm1.jpg">', '<img src="img/bm2.jpg">', '<img src="img/bm2.jpg">', '<img src="img/bm3.jpg">', '<img src="img/bm3.jpg">', '<img src="img/bm4.jpg">', '<img src="img/bm4.jpg">', '<img src="img/bm5.jpg">', '<img src="img/bm5.jpg">', '<img src="img/bm6.jpg">', '<img src="img/bm6.jpg">'];
+
 $(document).ready(function(){
   var cards = [];
   for (i = 0; i < 12; i++) {
-    var front = "<img src='img/bm1.jpg'>";
     var back = '<img src="img/card-back.jpg">';
-    var card = new Card(front, back);
+    var card = new Card(back);
+    card.randomize(images);
     cards.push(card);
   }
   cards.forEach(function(card) {
-    $('.container').append("<div class='card'>" + card.back + "</div>");
+    $('.container').append("<div class='card'>" + card.front + "</div>");
   });
 });
